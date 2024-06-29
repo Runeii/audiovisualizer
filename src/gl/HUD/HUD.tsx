288aabf69ed5265574dba9e21ca6e2b517289a97
Andrew Hill <contact@andrewthomashill.co.uk>
2024-06-29 12:33:41 +0200

    Add loudness levels to HUD

--- src/gl/HUD/HUD.tsx
+++ src/gl/HUD/HUD.tsx
@@ -1,11 +1,32 @@
+import { useMemo } from 'react';
 import useStore from '../../store';
 import styles from './HUD.module.css';
 import { Html } from "@react-three/drei"
+import { NUMBER_OF_PLAYERS } from '../constants';
 
 const HUD = () => {
+  const loudness = useStore(state => state.loudness);
   const tempo = useStore(state => state.tempo);
+
+  const positions = useMemo(() => {
+    // 24 bands of loudness 
+    const bandsPerPlayer = Math.round(24 / NUMBER_OF_PLAYERS);
+
+    return [...new Array(NUMBER_OF_PLAYERS)].map((_, index) => {
+      const startOfThisPlayerBands = (bandsPerPlayer * index);
+      const currentLoudnessOfPlayer = loudness.slice(startOfThisPlayerBands, startOfThisPlayerBands + bandsPerPlayer).reduce((acc, val) => acc + val, 0) / 6;
+
+      return (
+        <div key={index} className={styles.row}>
+          Ship {index + 1}: {Number(currentLoudnessOfPlayer).toFixed(2)}
+        </div>
+      )
+    })
+  }, [loudness]);
+
   return (
     <Html fullscreen visible wrapperClass={styles.hud}>
+      <div className={styles.positions}>{positions}</div>
       <div className={styles.speed}>{tempo ?? 0}<div className={styles.bpm}>bpm</div></div>
     </Html>
   );
