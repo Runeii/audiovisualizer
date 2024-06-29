62e2f1edd6f40218f5a7d02b75bf50776e925ce2
Andrew Hill <contact@andrewthomashill.co.uk>
2024-06-29 14:57:42 +0200

    Improve title screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { setupAudioProcessing, startAudioProcessing, stopAudioProcessing } from './audio/audio'
 import World from './gl/World'
-import useStore from './store'
 
 
 function App() {
   const [hasLaunched, setHasLaunched] = useState(false)
 
-  const handleClicked = async () => {
-    await setupAudioProcessing();
-    setHasLaunched(true);
-  }
+  useEffect(() => {
+    const handleClicked = async () => {
+      await setupAudioProcessing();
+      setHasLaunched(true);
+    }
+   
+    window.addEventListener('click', handleClicked)
+    return () => window.removeEventListener('click', handleClicked)
+  }, [])
 
   useEffect(() => {
     if (!hasLaunched) {
@@ -25,12 +29,13 @@ function App() {
     }
   }, [hasLaunched]);
 
-  const tempo = useStore(state => state.tempo)
   return (
     <>
-      <div className="title"><h1>Wipeout Music Visualizer</h1><br /><span class="subhead">Listening for audio</span></div>
-      <World className="world" />
-      {!hasLaunched && <button className="start" onClick={handleClicked}>Start</button>}
+      <div className={`title ${hasLaunched ? 'hasLaunched' : ''}`}>
+        <h1>Wipeout Music Visualizer</h1>
+        <span className="subhead">{hasLaunched ?'Listening for audio' : 'Click anywhere to start'}</span>
+      </div>
+      {hasLaunched && <World />}
     </>
   )
 }
